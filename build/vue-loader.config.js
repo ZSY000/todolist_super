2329f7c91fa353016a315cf07833628e0139efee
ZSY000 <xjl10050622@163.com>
2020-03-28 15:54:33 +0800

    格式

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -3,18 +3,15 @@
 module.exports = (isDev) => {
   return {
     preserveWhitepace: true, // 避免空格影响
-    extractCSS: !isDev,  // 提取css到一个文件中再加载，开发环境下不使用
+    extractCSS: !isDev, // 提取css到一个文件中再加载，开发环境下不使用
     cssModules: {
       localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
-      camelCase: true //把css中的用-连接的class类名转成驼峰命名
-    },
+      camelCase: true // 把css中的用-连接的class类名转成驼峰命名
+    }
     // hotReload: false // 热重载
-
-
-
     // 自定义loader，可以在组件中使用
     // loaders: {
-    //   'docs': docsLoader  
+    //   'docs': docsLoader
     // }
     // 在自定义loader解析完之前先用preLoader里的指定loader解析
     // preLoader: {},
